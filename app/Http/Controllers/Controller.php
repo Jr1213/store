07ab148a0295ff0890cc14bc42e8c5496bea6901
jr1213 <mohammed.jr.hammad@gmail.com>
2023-04-20 16:06:19 +0200

    categories

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function exceptionResponse(Exception $e): JsonResponse
+    {
+        return response()->json([
+            'success'   => false,
+            'message'   => $e->getMessage(),
+            'file'      => $e->getFile(),
+            'line'      => $e->getLine()
+        ], Response::HTTP_BAD_REQUEST);
+    }
+
+    public function successResponse(mixed $data, int $status = Response::HTTP_OK): JsonResponse
+    {
+        return response()->json([
+            'success'   => true,
+            'data'      => $data
+        ], $status);
+    }
 }
